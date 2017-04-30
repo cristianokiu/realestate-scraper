f5eb958dc87b9f91213503a47ffed6c269e34550
Paulo Romeira <paulo@pauloromeira.com>
2017-04-30 12:53:46 -0300

    Add mongodb pipeline

--- realestate/pipelines.py
+++ realestate/pipelines.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import pymongo
+from scrapy.conf import settings
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -8,4 +11,19 @@
 
 class RealestatePipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT'])
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+
+    def process_item(self, item, spider):
+        # TODO [romeira]: update if same id {25/04/17 20:14}
+        self.collection.insert(item)
         return item
--- realestate/settings.py
+++ realestate/settings.py
@@ -67,9 +67,10 @@ SPIDER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+ITEM_PIPELINES = {
+    'realestate.pipelines.MongoDBPipeline': 200,
 #    'realestate.pipelines.RealestatePipeline': 300,
-#}
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -91,3 +92,9 @@ AUTOTHROTTLE_ENABLED = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# MongoDB
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "realestate_scraper"
+MONGODB_COLLECTION = "realestate"
