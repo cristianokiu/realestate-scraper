9ceef7ae2b32b0c244b1860f5d2ff3af9b68f01c
Paulo Romeira <paulo@pauloromeira.com>
2017-06-11 18:46:42 -0300

    Add item_keys set and files

--- realestate/pipelines.py
+++ realestate/pipelines.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import pymongo
 import jmespath
+import pickle
+import pymongo
+import re
 
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from os import path
+from scrapy import signals
 
 
 class RealestatePipeline(object):
     def __init__(self, settings):
         expr = settings.get('JMESPATH')
         self.jmes = jmespath.compile(expr) if expr else None
+        self.item_keys = set()
+        self.item_keys_filename = 'item_keys.pickle'
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(crawler.settings)
+        o = cls(crawler.settings)
+        crawler.signals.connect(o.spider_closed, signal=signals.spider_closed)
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
 
     def process_item(self, item, spider):
         scraped = spider.crawler.stats.get_value('scraped_pages')
         total = spider.crawler.stats.get_value('selected_pages')
+
+        titulo = item.get('TituloPagina')
+        if titulo:
+            item['TituloPagina'] = re.sub('(?i)\s*-\s*zap\s*im[óo]veis\s*',
+                                          '', titulo)
 
         spider.logger.info('Pages: [{}/{}] - {:.0%}'.format(
             scraped, total, scraped/total))
@@ -28,8 +38,23 @@ class RealestatePipeline(object):
             spider.crawler.stats.get_value('item_scraped_count') or 0,
             item.get('TituloPagina')))
 
+        self.item_keys.update(item.keys())
+
         return self.jmes.search(item) if self.jmes else item
 
+    def spider_opened(self, spider):
+        if path.isfile(self.item_keys_filename):
+            with open(self.item_keys_filename, 'rb') as f:
+                self.item_keys.update(pickle.load(f))
+
+    def spider_closed(self, spider):
+        if self.item_keys:
+            with open(self.item_keys_filename, 'wb') as f:
+                pickle.dump(self.item_keys, f)
+            with open('jmespath.txt', 'w') as f:
+                f.write('{\n')
+                f.writelines('  {0}:{0}\n'.format(k) for k in self.item_keys)
+                f.write('}')
 
 class MongoDBPipeline(object):
     @classmethod
