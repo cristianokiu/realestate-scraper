572d702177fc5df9e2c98b5e65dc71b613c6e148
Paulo Romeira <paulo@pauloromeira.com>
2017-06-11 16:40:31 -0300

    Enable cache

--- realestate/settings.py
+++ realestate/settings.py
@@ -49,7 +49,6 @@ COOKIES_ENABLED = False
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 SPIDER_MIDDLEWARES = {
-    'scrapy.downloadermiddlewares.httpcache.HttpCacheMiddleware': 200,
 #    'realestate.middlewares.RealestateSpiderMiddleware': 543,
 }
 
@@ -87,7 +86,7 @@ AUTOTHROTTLE_ENABLED = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = True
 #HTTPCACHE_EXPIRATION_SECS = 0
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
